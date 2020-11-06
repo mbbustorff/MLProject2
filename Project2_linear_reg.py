fd3d0c1f7d03ddbbf9c8efc476faa039f95cfae0
S163669 <48013307+S163669@users.noreply.github.com>
2020-11-06 21:18:43 +0100

    Create Project2_linear_reg.py

---
+++ Project2_linear_reg.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 14:47:18 2020
+
+@author: cleml
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import torch
+from sklearn import model_selection
+from toolbox_02450 import train_neural_net, draw_neural_net
+from scipy import stats
+import sklearn.linear_model as lm
+from toolbox_02450 import rlr_validate
+
+#CHANGE optimizer = torch.optim.Adam(net.parameters(),weight_decay=1e-5)
+#IN train_neural_net BY REMOVING weight_decay TO NOT DO REGULARIZATION
+lr_ord = 2
+# Loading data
+filename = 'heart.csv'
+df = pd.read_csv(filename)
+# Making one-of-K encoding of categorical data
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+# Extracting target subdataframe from data frame
+targ = df.pop('target')
+# Concatenating current data frame and encoding one-of-k values and
+# making sure target is the  last attribute of the data frame
+frames = [df, a, b, c, targ]
+df = pd.concat(frames, axis = 1)
+# Dropping original form of one-of-K encoded variables from dataframe
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+# Getting attributes names
+attributeNames = list(df.columns)
+attNb = len(attributeNames)
+#t = attributeNames.index('target')
+#converting data to arrays
+#data = df.to_numpy()
+#X = data[:,:len(data[0,:])-1]
+# splitting dataframe into X and y
+X = df.drop(['target'], axis = 1).to_numpy()
+y = df.target.values.reshape((len(X),1))
+#y = data[:,len(data[0,:])-1].reshape((len(X),1))
+
+#Eventually add: ,'restecg','exang','sex','fbs'
+df_reg = df[['age','oldpeak','ca','trestbps','chol']]
+attributeNames_reg = list(df_reg.columns)
+X_reg = df_reg.to_numpy()
+y_reg = df[['thalach']].to_numpy().squeeze()
+
+
+model = lm.LinearRegression()
+model.fit(X_reg,y_reg)
+
+# Predict alcohol content
+y_est = model.predict(X_reg)
+residual = y_est-y_reg
+
+# Display scatter plot
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(y_reg, y_est, '.')
+plt.xlabel('Alcohol content (true)'); plt.ylabel('Alcohol content (estimated)');
+plt.subplot(2,1,2)
+plt.hist(residual,40)
+
+plt.show()
+
+N, M = X_reg.shape
+
+# Add offset attribute
+X_reg = np.concatenate((np.ones((X_reg.shape[0],1)),X_reg),1)
+attributeNames_reg = [u'Offset']+attributeNames_reg
+M = M+1
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 5
+CV = model_selection.KFold(K, shuffle=True)
+#CV = model_selection.KFold(K, shuffle=False)
+
+# Values of lambda
+lambdas = np.power(10.,range(-5,9))
+#lambdas = np.linspace(0e-2,1000,1000)
+
+# Initialize variables
+#T = len(lambdas)
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_rlr = np.empty((K,1))
+Error_test_rlr = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
+w_rlr = np.empty((M,K))
+mu = np.empty((K, M-1))
+sigma = np.empty((K, M-1))
+w_noreg = np.empty((M,K))
+
+k=0
+for train_index, test_index in CV.split(X_reg,y_reg):
+    
+    # extract training and test set for current CV fold
+    X_train = X_reg[train_index]
+    y_train = y_reg[train_index]
+    X_test = X_reg[test_index]
+    y_test = y_reg[test_index]
+    internal_cross_validation = 10    
+    
+    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+
+    # Standardize outer fold based on training set, and save the mean and standard
+    # deviations since they're part of the model (they would be needed for
+    # making new predictions) - for brevity we won't always store these in the scripts
+    mu[k, :] = np.mean(X_train[:, 1:], 0)
+    sigma[k, :] = np.std(X_train[:, 1:], 0)
+    
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+    
+    # Compute mean squared error without using the input data at all
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = opt_lambda * np.eye(M)
+    lambdaI[0,0] = 0 # Do no regularize the bias term
+    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
+
+    # Estimate weights for unregularized linear regression, on entire training set
+    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+    # Compute mean squared error without regularization
+    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+    #m = lm.LinearRegression().fit(X_train, y_train)
+    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    # Display the results for the last cross-validation fold
+    if k == K-1:
+        plt.figure(k, figsize=(12,8))
+        plt.subplot(1,2,1)
+        plt.semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Mean Coefficient Values')
+        plt.grid()
+        # You can choose to display the legend, but it's omitted for a cleaner 
+        # plot, since there are many attributes
+        #legend(attributeNames[1:], loc='best')
+        
+        plt.subplot(1,2,2)
+        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+        plt.loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Squared error (crossvalidation)')
+        plt.legend(['Train error','Validation error'])
+        plt.grid()
+    
+    # To inspect the used indices, use these print statements
+    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
+    #print('Train indices: {0}'.format(train_index))
+    #print('Test indices: {0}\n'.format(test_index))
+
+    k+=1
+
+plt.show()
+# Display results
+print('Linear regression without feature selection:')
+print('- Training error: {0}'.format(Error_train.mean()))
+print('- Test error:     {0}'.format(Error_test.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+print('Regularized linear regression:')
+print('- Training error: {0}'.format(Error_train_rlr.mean()))
+print('- Test error:     {0}'.format(Error_test_rlr.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
+
+print('Weights in last fold:')
+for m in range(M):
+    print('{:>15} {:>15}'.format(attributeNames_reg[m], np.round(w_rlr[m,-1],2)))
