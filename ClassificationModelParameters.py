31b6a9aa9eb70dd7861d7288fbb67287a05923b3
mbbustorff <63663419+mbbustorff@users.noreply.github.com>
2020-11-15 15:06:06 +0100

    still need to fix logistic reg.

--- ClassificationModelParameters.py
+++ ClassificationModelParameters.py
@@ -20,6 +20,7 @@ from matplotlib.pylab import figure, plot, xlabel, ylabel, legend, show, boxplot
 from sklearn import model_selection, tree
 import sklearn.linear_model as lm
 from Functions import rlr_validate
+from decimal import Decimal
 
 """
 --------------------------------------------
@@ -94,8 +95,7 @@ for train_index, test_index in CV.split(X):
     
     for i in np.where(sigma==0)[0]:
             sigma[i] = 1
-
-    print("Difference sigma: {:}".format(sigma-sigmabis))
+            
     for i, t in enumerate(tc):
         # Fit decision tree classifier, Gini split criterion, different pruning levels
         dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t)
@@ -149,57 +149,66 @@ attributeNames_reg = list(df_reg.columns)
 X_reg = df_reg.to_numpy()
 y_reg = df[['thalach']].to_numpy().squeeze()
 
+# K-fold crossvalidation
+K = 10
+CV = model_selection.KFold(n_splits=K,shuffle=True, random_state=12)
 
-# Create crossvalidation partition for evaluation
-# using stratification and 95 pct. split between training and test 
-K = 20
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.80, stratify=y)
-# Try to change the test_size to e.g. 50 % and 99 % - how does that change the 
-# effect of regularization? How does differetn runs of  test_size=.99 compare 
-# to eachother?
+k=0
+for train_index, test_index in CV.split(X):
+    print('Computing CV fold: {0}/{1}..'.format(k+1,K))
 
-# Standardize the training and set set based on training set mean and std
-mu = np.mean(X_train, 0)
-sigma = np.std(X_train, 0)
-
-X_train = (X_train - mu) / sigma
-X_test = (X_test - mu) / sigma
-
-# Add offset attribute
-X_reg = np.concatenate((np.ones((X_reg.shape[0],1)),X_reg),1)
-attributeNames_reg = [u'Offset']+attributeNames_reg
-M = M+1
-
-# Fit regularized logistic regression model to training data to predict 
-# the type of wine
-lambda_interval = np.logspace(-8, 2, 50)
-train_error_rate = np.zeros(len(lambda_interval))
-test_error_rate = np.zeros(len(lambda_interval))
-coefficient_norm = np.zeros(len(lambda_interval))
-for k in range(0, len(lambda_interval)):
-    mdl = LogisticRegression(penalty='l2', C=1/lambda_interval[k] )
+    # extract training and test set for current CV fold
+    X_train, y_train = X[train_index,:], y[train_index]
+    X_test, y_test = X[test_index,:], y[test_index]
     
-    mdl.fit(X_train, y_train) #is sometimes problematic (input NaN)
-
-    y_train_est = mdl.predict(X_train).T
-    y_test_est = mdl.predict(X_test).T
+    # Standardize outer fold based on training set
+    mu = np.mean(X_train[:, 1:], 0)
+    sigma = np.std(X_train[:, 1:], 0)
     
-    train_error_rate[k] = np.sum(y_train_est != y_train) / len(y_train)
-    test_error_rate[k] = np.sum(y_test_est != y_test) / len(y_test)
+    X_train[:, 1:] = (X_train[:, 1:] - mu ) / sigma
+    X_test[:, 1:] = (X_test[:, 1:] - mu ) / sigma
+    
+    
+    for i in np.where(sigma==0)[0]:
+            sigma[i] = 1
+    
+    # Add offset attribute
+    X_reg = np.concatenate((np.ones((X_reg.shape[0],1)),X_reg),1)
+    attributeNames_reg = [u'Offset']+attributeNames_reg
+    M = M+1
+    
+    # Fit regularized logistic regression model to training data to predict 
+    # the type of wine
+    lambda_interval = np.logspace(-8, 2, 50)
+    train_error_rate = np.empty((len(lambda_interval),K))
+    test_error_rate = np.empty((len(lambda_interval),K))
+    coefficient_norm = np.empty((len(lambda_interval),K))
+    for j in range(0, len(lambda_interval)):
+        mdl = LogisticRegression(penalty='l2', C=1/lambda_interval[j], random_state=10 )
+        
+        mdl.fit(X_train, y_train) #is sometimes problematic (input NaN)
+    
+        y_train_est = mdl.predict(X_train).T
+        y_test_est = mdl.predict(X_test).T
+        
+        train_error_rate[j,k] = np.sum(y_train_est != y_train) / float(len(y_train_est))
+        test_error_rate[j,k] = np.sum(y_test_est != y_test) / float(len(y_test_est))
+    
+        w_est = mdl.coef_[0] 
+        coefficient_norm[j] = np.sqrt(np.sum(w_est**2))
+    k+=1
+    
+opt_lambda_idx=np.argmin(test_error_rate.mean(1))
+min_error=test_error_rate.mean(1)[opt_lambda_idx]
+opt_lambda=lambda_interval[opt_lambda_idx]
 
-    w_est = mdl.coef_[0] 
-    coefficient_norm[k] = np.sqrt(np.sum(w_est**2))
-
-min_error = np.min(test_error_rate)
-opt_lambda_idx = np.argmin(test_error_rate)
-opt_lambda = lambda_interval[opt_lambda_idx]
 
 fig=plt.figure(figsize=(8,8))
 #plt.plot(np.log10(lambda_interval), train_error_rate*100)
 #plt.plot(np.log10(lambda_interval), test_error_rate*100)
 #plt.plot(np.log10(opt_lambda), min_error*100, 'o')
-plt.semilogx(lambda_interval, train_error_rate*100)
-plt.semilogx(lambda_interval, test_error_rate*100)
+plt.semilogx(lambda_interval, test_error_rate.mean(1)*100)
+plt.semilogx(lambda_interval, test_error_rate.mean(1)*100)
 plt.semilogx(opt_lambda, min_error*100, 'o')
 plt.text(1e-8, 3, "Minimum test error: " + str(np.round(min_error*100,2)) + ' % at 1e' + str(np.round(np.log10(opt_lambda),2)))
 plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
@@ -210,7 +219,7 @@ plt.ylim([0, 30])
 plt.grid()
 plt.show()  
 fig.savefig('plot.pdf')  
-
+"""
 plt.figure(figsize=(8,8))
 plt.semilogx(lambda_interval, coefficient_norm,'k')
 plt.ylabel('L2 Norm')
@@ -218,6 +227,7 @@ plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
 plt.title('Parameter vector L2 norm')
 plt.grid()
 plt.show()    
-
+"""
 print("LOGISTIC REGRESSION: ")
-print('Best error achieved is {:} for the parameter: {:}.'.format(min_error,opt_lambda))
+lambda_answer=f"{opt_lambda:.2E}"
+print('Best error achieved is {:} for the parameter: {:}.'.format(min_error*100, lambda_answer))
